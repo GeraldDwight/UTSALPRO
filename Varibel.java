b7df1ab3bb150147aa2a0a3c808b5fd8f5f33000
GeraldDwight <115127480+GeraldDwight@users.noreply.github.com>
2022-11-23 21:51:41 -0700

    Update Varibel.java

--- Varibel.java
+++ Varibel.java
@@ -1,8 +1,8 @@
 public class uts5 {
-    public static void main(String args[]) {
-      String helo = "Assalamualikum";
-      String nama = "Gerald Dwight Andigha Cardinsyah";
-      int tahun = 2003;
+    public  static  void  main ( String  args []) {
+      String  helo = "Assalamualikum";
+      String  nama = "Gerald Dwight Andigha Cardinsyah";
+      int  tahun = 2003 ;
 
       System.out.println(helo);
       System.out.println("Nama Saya = " + nama);
